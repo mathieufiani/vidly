f58f31fd5176055212ba9134997139ad88a06feb
Mathieu Fiani <mathieu.fiani2@gmail.com>
2021-09-07 13:54:36 +0200

    table disappear when no films stores

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,26 +3,30 @@ import Movie from "./Movie";
 class Movies extends Component {
   renderComponents() {
     const { onDelete, movies, titles } = this.props;
-    return (
-      <React.Fragment>
-        <table className="table">
-          <thead>
-            <tr>
-              {titles.map((title) => (
-                <th scope="col" key={title}>
-                  {title}
-                </th>
+    if (movies.length !== 0) {
+      return (
+        <React.Fragment>
+          <table className="table">
+            <thead>
+              <tr>
+                {titles.map((title) => (
+                  <th scope="col" key={title}>
+                    {title}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map((movie) => (
+                <Movie key={movie._id} movie={movie} onDelete={onDelete} />
               ))}
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map((movie) => (
-              <Movie key={movie._id} movie={movie} onDelete={onDelete} />
-            ))}
-          </tbody>
-        </table>
-      </React.Fragment>
-    );
+            </tbody>
+          </table>
+        </React.Fragment>
+      );
+    } else {
+      return <React.Fragment></React.Fragment>;
+    }
   }
   render() {
     return this.renderComponents();

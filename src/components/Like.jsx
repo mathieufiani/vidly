6fe676f53d27f88c5bab71d7781f5792503962a1
Mathieu Fiani <mathieu.fiani2@gmail.com>
2021-09-07 15:37:48 +0200

    common

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 
 class Like extends Component {
   render() {
+    const { onLike, movie } = this.props;
     return (
-      <button
-        className="btn btn-default"
-        onClick={() => this.props.onLike(this.props.movie)}
-      >
+      <button className="btn btn-default" onClick={() => onLike(movie)}>
         <i
-          className={this.props.movie.like ? "fa fa-heart" : "fa fa-heart-o"}
+          className={movie.like ? "fa fa-heart" : "fa fa-heart-o"}
           aria-hidden="true"
         ></i>
       </button>
